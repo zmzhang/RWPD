dc5b9e5da8e4897354a20a47fc86afdfa4300e2c
zmzhang <zmzhang@csu.edu.cn>
2015-08-07 11:49:47 +0800

    shape

---
+++ R/peakshape.R
@@ -0,0 +1,11 @@
+peakshape <- function(x,position,width,shape){
+	y=seq(1,1,1)
+	if (shape==1){
+			y=exp(-((x-position)/(0.6005612*width))^2);
+		}
+	}
+	else{
+			y=1/(1+((x-position)/(0.5*width))^2)
+	}
+	return(y)
+}
