5f3d5a2d15d0734629e9942493e9b801ca0a981a
zmzhang <zmzhang@csu.edu.cn>
2015-08-09 23:00:20 +0800

    points

--- R/plotwidthEstimation.R
+++ R/plotwidthEstimation.R
@@ -3,12 +3,12 @@ plotwidthEstimation <- function(x,y,peakWidth) {
     lmindex=1:length(y)
     peakIndex=peakWidth$peakIndex
     LR = NULL 
-	plot(x,y,type='l')     
+	# plot(x,y,type='l')     
     points(x[peakIndex],y[peakIndex],pch=15)
     for (i in 1:length(peakIndex)){
         peakWidth.i=peakWidth$peakIndexLower[i]:peakWidth$peakIndexUpper[i]
         LR=c(LR,peakWidth.i[c(1,length(peakWidth.i))])
     }
-	points(LR,x[LR])
+	points(x[LR],y[LR])
     return ("successful")
 }
