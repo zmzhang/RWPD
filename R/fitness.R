50c2afb1c92f3a7bbc2724a4ba87249e9b65faa9
zmzhang <zmzhang@csu.edu.cn>
2015-09-18 16:29:52 +0800

    name

--- R/fitness.R
+++ R/fitness.R
@@ -1,19 +1,3 @@
-library(GA)
-fitshape <- function(lambda,x,y,shape){
-	# lambda <- as.vector(t(cbind(position,width)));
-	A <- matrix(0,length(x),round(length(lambda)/2))
-	for(j in 1:(length(lambda)/2))
-	{
-		position <-lambda[2*j-1] ; width <- lambda[2*j];
-		A[ ,j] <- peakshape(x,lambda[2*j-1],lambda[2*j],shape);
-	}
-	lf=lsfit(A,y)
-	PEAKHEIGHTS=abs(lf$coef[-1])
-	e=y-A%*%PEAKHEIGHTS
-	return(base::norm(as.matrix(e),'2'))
-}
-
 fitness <- function(lambda,x,y,shape){
 	-fitshape(lambda,x,y,shape)
 }
-
